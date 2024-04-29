3620d7cce41e74ea0b657070daea187c30545734
Zubayer24 <159532966+Zubayer24@users.noreply.github.com>
2024-04-29 14:43:20 +0600

    Add files via upload

---
+++ Project-2.py
@@ -0,0 +1,8 @@
+Username = input("Your name:")
+fathername = input("Your father name:")
+mothername = input("Your mother name:")
+study = input("I'm in class:")
+relationship = input("Your relationship status:")
+
+print(f"My doggy name is {Username},My doggys father name {fathername},my doggys mother name is {mothername},"
+      f"my doggy studying in class {study},Our relationship is {relationship}")
